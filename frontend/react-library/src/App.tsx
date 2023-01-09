7b6d77c15ef9fe6e2e6f119951a20e03e4b9f134
Shuyang Wang <45084681+sherry122232@users.noreply.github.com>
2023-01-08 21:40:55 -0500

    Add files via upload

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { HomePage } from './layouts/HomePage/HomePage';
+
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar/><HomePage/><Footer/>
+    </div>
+  );
+}
